598065aa6cc6faff61c18f50f3a39c96228a78a5
Hassaan <h.n.ghayas@gmail.com>
2024-12-09 18:18:43 +0500

    fixing vercel errors

--- my-app/src/app/components/homeHero.tsx
+++ my-app/src/app/components/homeHero.tsx
@@ -46,7 +46,7 @@ export default function HomeHeroSection() {
               <path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z" />
             </svg>
           </div>
-      <div key={2} className="w-full grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-5 relative max-w-screen-xl m-auto">
+      <div key={4041} className="w-full grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-5 relative max-w-screen-xl m-auto">
         {heroSecs.map((hero, index) => {
           return (
             <>
